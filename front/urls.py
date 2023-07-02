291ec179deabf14c84f5f4ead2da2812b9fcd9de
diegoesteban97 <dalchahuene@gmail.com>
2023-07-02 00:22:14 -0400

    modified

--- front/urls.py
+++ front/urls.py
@@ -1,7 +1,9 @@
 #from django.conf.urls import url
+from django.contrib import admin
 from django.urls import path
 from  . import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('index', views.index, name='index'),
 ]
