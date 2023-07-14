26cadb9d57818b0b6baa3be3360a105b309c9779
AlexZander25 <aledelflou@hotmail.cl>
2023-07-13 21:55:38 -0400

    crud

--- administrador/models.py
+++ administrador/models.py
@@ -4,8 +4,9 @@ from django.db import models
 class Producto(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=50)
-    precio = models.IntegerField(default=0)
+    precio = models.IntegerField()
     stock = models.IntegerField(default=0)
+
 
 class User(models.Model):
     usuario = models.CharField(max_length=20, primary_key=True)
@@ -14,3 +15,7 @@ class User(models.Model):
     correo = models.CharField(max_length=30)
     telefono = models.IntegerField()
     fechaRegistro = models.DateTimeField(auto_now_add=True)
+
+
+
+    
--- administrador/urls.py
+++ administrador/urls.py
@@ -7,9 +7,11 @@ urlpatterns = [
     path('calendario', views.calendario, name='calendario'),
     path('explora', views.buscar, name='explora'),
     path('login', views.login, name='login'),
-    path('mantenedor', views.mantenedor, name='mantenedor'),
+    path('mantenedor', views.crud, name='mantenedor'),
     path('noticias', views.noticias, name='noticias'),
     path('producto', views.productos, name='producto'),
     path('registro', views.registrarse, name='registro'),
     path('videos', views.videos, name='videos'),
+    path('crud',views.crud,name='crud'),
+    path('mantenedor_del/<str:pk>', views.mantenedor_del, name='mantenedor_del'),
 ]
